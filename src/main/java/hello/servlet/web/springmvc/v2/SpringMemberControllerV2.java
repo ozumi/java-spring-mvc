5b757ac881a41aaff7b0d31d3c1815d37d13ddd6
Juyoung Oh <ojmme625@gmail.com>
2023-02-22 00:27:48 +0900

    Integrate controller using Spring MVC

--- src/main/java/hello/servlet/web/springmvc/v2/SpringMemberControllerV2.java
+++ src/main/java/hello/servlet/web/springmvc/v2/SpringMemberControllerV2.java
@@ -1,24 +1,36 @@
-package hello.servlet.web.springmvc.v1;
+package hello.servlet.web.springmvc.v2;
 
 import hello.servlet.domain.member.Member;
 import hello.servlet.domain.member.MemberRepository;
-import hello.servlet.web.frontcontroller.ModelView;
-import jakarta.servlet.RequestDispatcher;
-import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-import java.util.Map;
+import java.util.List;
 
 @Controller
-public class SpringMemberSaveControllerV1 {
+@RequestMapping("/springmvc/v2/members")
+public class SpringMemberControllerV2 {
     MemberRepository memberRepository = MemberRepository.getInstance();
 
-    @RequestMapping("/springmvc/v1/members/save")
-    public ModelAndView process(HttpServletRequest request, HttpServletResponse response) {
+    @RequestMapping("/new-form")
+    public ModelAndView newForm() {
+        return new ModelAndView("new-form");
+    }
+    
+    @RequestMapping
+    public ModelAndView members() {
+        List<Member> members = memberRepository.findAll();
+        ModelAndView modelView = new ModelAndView("members");
+        modelView.addObject("members", members);
+
+        return modelView;
+    }
+
+    @RequestMapping("/save")
+    public ModelAndView save(HttpServletRequest request, HttpServletResponse response) {
         String username = request.getParameter("username");
         int age = Integer.parseInt( request.getParameter("age"));
 
